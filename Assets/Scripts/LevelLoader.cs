ac9d97e54f0f443f7348b9bc12b11769599ed927
AnnPla <anna.platek2222@gmail.com>
2019-06-23 10:33:12 +0200

    Created splash screen, added music and loading next level

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelLoader : MonoBehaviour {
+
+    void Awake()
+    {
+        DontDestroyOnLoad(transform.gameObject);
+    }
+    
+	// Use this for initialization
+	void Start () {
+        Invoke("LoadFirstLevel", 4f);
+	}
+	
+    void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(1);       
+    }
+}
