03f6c458f77322715dc359cd2da10bc4e9c73416
AnnPla <anna.platek2222@gmail.com>
2019-07-28 20:31:48 +0200

    Player's ship movement along x and y axes

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 4f;
+    [Tooltip("In m")] [SerializeField] float xRange = 10f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float HorizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float VerticalThrow = CrossPlatformInputManager.GetAxis("Vertical");
+        //print(HorizontalThrow);
+        float xOffset = Time.deltaTime * HorizontalThrow * xSpeed;
+        float yOffset = Time.deltaTime * VerticalThrow * xSpeed;
+        //print(xOffset);
+        float rawNewXPos = transform.localPosition.x + xOffset;
+        float rawNewYPos = transform.localPosition.y + yOffset;
+
+        transform.localPosition = new Vector3(Mathf.Clamp(rawNewXPos, -xRange, xRange), 
+            Mathf.Clamp(rawNewYPos,-4,4), 
+            transform.localPosition.z);
+    }
+}
