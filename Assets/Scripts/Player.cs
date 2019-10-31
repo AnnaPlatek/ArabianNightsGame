ebfbacbaef41f66cd0fb33bc80e632df20e1b272
AnnPla <anna.platek2222@gmail.com>
2019-10-31 19:38:12 +0100

    Add box collider and rigidbody to player ship and enemies

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -23,6 +23,16 @@ public class Player : MonoBehaviour
         
     }
 
+    void OnCollisionEnter(Collision collision)
+    {
+        print("Player collided with something");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print("Player triggered something");
+    }
+
     // Update is called once per frame
     void Update()
     {
